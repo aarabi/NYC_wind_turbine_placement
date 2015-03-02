174037ce4ea2d0090e395bb16947682e331f605c
Aarabi <aarabi.madhavan@gmail.com>
2015-03-02 11:28:53 -0500

    case 1, 2, 3

--- weekend.r
+++ weekend.r
@@ -2,7 +2,7 @@ library(gdata)
 library(lpSolveAPI)
 
 setwd("R:/projects/1 weekend thing")
-
+load("data_storage.RData")
 
 power_per_site<-read.csv("sites.wind.power.per.csv", header=TRUE)
 wind_sites<-read.csv("wind.sites.csv", header=TRUE)
@@ -85,7 +85,7 @@ for(a in seq(from=6, to=11, by=1))
   }
 }
 turbine.cap <- c(3, 3000, 6000, 9000, 12000, 15000)
-apply(ranking[11:16],2,sum)/(turbine.cap*8760)
+cap.factor.wind1a<-apply(ranking[11:16],2,sum)/(turbine.cap*8760)
 
 
 #CASE 1B
@@ -135,14 +135,16 @@ for(m in 5:10)
 
 
 turbine.cap <- c(3, 3000, 6000, 9000, 12000, 15000)
-apply(sum[,7:12],2,sum)/(turbine.cap*8760)
+cap.factor.wind1b<-apply(sum[,7:12],2,sum)/(turbine.cap*8760)
 
 # CASE 2
 sites_no <- 66
 sites <- wind_sites$site
 turb_cap <- c(3, 3000, 6000, 9000, 12000, 15000)
 times <- 8760
-
+turbine.distribution2<-matrix(list(), nrow=6, ncol=66)
+other.gen.total.mwh2<-matrix(list(),nrow=6)
+cap.factor.wind2<-matrix(list(),nrow=6)
 
 for (k in 1:length(turb_cap))
   {
@@ -172,13 +174,102 @@ for (k in 1:length(turb_cap))
   print(turbine.distribution)
   
   print("Total Wind Cap")
-  wind_cap <- (sum(demand$state)- solution - state_power*times)/(wind*8760)
-  print(wind_cap)
+  turbine.distribution2[k,] <- get.variables(optimize)[1:sites_no]
+  other.gen.total.mwh2[k] <- get.objective(optimize)
   
-  #print("Optimized Solution")
-  #print(solution)
- 
+  cap.factor.wind2[k] <- (sum(demand$state)- as.integer(other.gen.total.mwh2[k]) - state_power*times)/(wind*8760)
+  
   write.lp(optimize,'model.lp',type='lp')
 }
 
 
+#case 3
+sites.west <- wind_sites$site[which(wind_sites$region=="west")]
+sites.east <- wind_sites$site[which(wind_sites$region=="east")]
+sites.downstate <- wind_sites$site[which(wind_sites$region=="downstate")]
+sites <- wind_sites$site
+n.sites.west <- length(sites.west)
+n.sites.east <- length(sites.east)
+n.sites.downstate <- length(sites.downstate)
+
+#now we have the 5 variables and the 3 regions 
+n.var <- 66 + 5*8760
+n.con<- 1+ 3*8760
+n.bo <- 66 + 8760
+n.sites<-66
+n.hrs<-8760
+trans.limit.east <- trans$trans.limit.mw[1]
+trans.limit.downstate <- trans$trans.limit.mw[2]
+turbine.capacities <- c(3, 3000, 6000, 9000, 12000, 15000)
+turbine.distribution3<-matrix(list(), nrow=6, ncol=66)
+other.gen.total.mwh3<-matrix(list(),nrow=6)
+cap.factor.wind3<-matrix(list(),nrow=6)
+for (j in 1:length(turbine.capacities)){
+  wind.cap.total.mw3 <- turbine.capacities[j]
+  rate.cap.per.mw <- 3
+  
+  n.t <- wind.cap.total.mw3/rate.cap.per.mw 
+  
+  #develop MILP:
+  lp.case3 <- make.lp(nrow = n.con, ncol = n.var)
+  lp.control(lp.case3, sense = 'min')
+  #set coefficients
+  set.objfn(lp.case3,obj=c(rep(0,n.sites), rep(1,3*n.hrs),rep(0,2*n.hrs)))
+  
+  
+  #force number of turbines at each site to be an integer
+  set.type(lp.case3, c(1:n.sites),type = "integer")
+  
+  #add constraints by row (rather than by column)
+  #turn on row entry mode - turn off after adding all constraints
+  row.add.mode(lp.case3, "on")
+  add.constraint(lp.case3, xt= rep(1,n.sites), type="=", rhs=n.t, indices=c(1:n.sites))
+  
+  
+  #energy balance: West
+  for(i in 1:n.hrs){
+    add.constraint(lp.case3,xt=c(power_per_site[i,1+sites.west],1,(-1)),type=">=", rhs=demand[i, "west"] - west_power, indices=c(sites.west,(n.sites+i),(n.sites+3*n.hrs+i)))
+    
+  }
+  #west.capgen.mw = nonbase
+  
+  
+  #energy balance: East
+  for(i in 1:n.hrs){
+    add.constraint(lp.case3,xt=c(power_per_site[i,1+sites.east],1,1,(-1)),type=">=", rhs=demand[i,"east"] - east_power, indices=c(sites.east,(n.sites+n.hrs+i),(n.sites+3*n.hrs+i),(n.sites+4*n.hrs+i)))
+  }
+  
+  #energy balance: Downstate
+  for(i in 1:n.hrs){
+    add.constraint(lp.case3,xt=c(power_per_site[i,1+sites.downstate],1,1),type=">=", rhs=demand[i, "downstate"] - downstate_power, indices=c(sites.downstate,(n.sites+2*n.hrs+i),(n.sites+4*n.hrs+i)))
+  }
+  
+  #remember to turn off row entry mode
+  row.add.mode(lp.case3, "off")
+  
+  #set bounds on decision variables
+  set.bounds(lp.case3, lower=rep(0,n.var), upper= c(wind_sites$n.t.max,rep(west_cap,n.hrs),rep(east_cap,n.hrs),rep(down_cap,n.hrs),rep(trans.limit.east,n.hrs),rep(trans.limit.downstate,n.hrs)))
+  
+  
+  #solves optimization problem:
+  solve(lp.case3)
+  turbine.distribution3[j,] <- get.variables(lp.case3)[1:n.sites]
+  other.gen.total.mwh3[j] <- sum(get.variables(lp.case3)[67:26346])
+  #now input for below
+  cap.factor.wind3[j] <- (sum(demand$state) - as.integer(other.gen.total.mwh3[j]) -state_power*n.hrs)/(wind.cap.total.mw3*8760)
+  
+  
+  
+  
+}
+turbine.caps <- c(3, 3000, 6000, 9000, 12000, 15000)
+require(ggplot2)
+save(cap.factor.wind1b,cap.factor.wind1a,cap.factor.wind2,cap.factor.wind3,file="data_storage.RData")
+ggplot() +
+  geom_line(aes(x=turbine.caps, y= cap.factor.wind1a, color = "Overall vs.Installed 1A"),)  +
+  geom_line(aes(x=turbine.caps, y = cap.factor.wind1b, color = "Curtailed vs. Installed 1B"),) +
+  geom_line(aes(x=turbine.caps, y = unlist(cap.factor.wind2), color = "Case2"),) +
+  geom_line(aes(x=turbine.caps, y =unlist(cap.factor.wind3), color = "case 3"),) +
+  ylab("Capacity Factor") +
+  xlab("Installed Total Capacity") +
+  theme(legend.position="right",legend.title=element_blank())
